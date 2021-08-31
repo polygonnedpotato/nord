e130efa60df4811039c26b2c3776ab9a7316f0c0
elburg the polygonned potato <73083772+polygonnedpotato@users.noreply.github.com>
2021-08-31 16:41:46 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "What should nord say?\ninput:";
+  string nord;
+  cin >> nord;
+  cout << "nord says: \t";
+  cout << nord;
+  cout << "\n";
+}
