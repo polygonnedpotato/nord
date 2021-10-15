dbe042ab85f7871d2c7c8dc3167fa700dded3ccb
Discovery <73083772+polygonnedpotato@users.noreply.github.com>
2021-10-15 16:52:26 +0000

    yes

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 class RES{
   public:
-    string game_dat[4]={"\e[1mOPTIONS:\n\e[22m","\e[3m\e[1m[i] You can't select this option...\e[0m\n","E00:INCORRECT_SCENE_INTEGER","E01:INVALID_INTEGER"};
-    string scn_00[2]={"\e[36m[?]Hey...\n[?]Hey!\n\e[1m[?]HEY!!!\n\e[0m\e[35m<You wake up suddenly.>\n<You look up and notice a larg dark figure. It speaks in a masculine voice...>\e[0m\e[36m\n[?]Come with me,\e[1m now\e[22m.\n\e[0m","\e[32m[1]O-Okay...\n\e[31m[2]Umm... No?\e[0m\n[3]Who are you?\n"};
-    string scn_01[2]={"\e[36m[?]I'm the guy who's trying to save you. Now follow me!\n\e[0m","\e[32m[1]Okay\n\e[31m[2]No.\n\e[0m\e[2m[3]-----\n\e[0m"};
-    string scn_02[2]={"\e[36m[?]Are you joking? Now is not the time for this! Especially considering that \e[1mthe sweep\e[22m is happening. Now come on!\e[0m\n","\e[32m[1]Okay...\n\e[0m[2]Wait what's 'the sweep'?\n[3]Who are you?\n"};
-    string scn_03[2]={"\e[35m<The dark figure pulls you up without patience.>\n<You find it hard to stabalize yourself on your feet.>\n<Without hesitation, the figure grabs ahold of your arm and pulls you along with itself.>\n<As the figure pulls you to an undisclosed location, you look around. Destrucion, fire, debris. it looks as if the entire place was severly bombed.>\n<You feel light headed.>\n<You blackout.>\e[0m\n","\e[33m[1]Continue\e[0m\n\e[2m[2]-----\n[3]-----\e[0m\n"};
+    const string game_dat[5]={"\e[1mOPTIONS:\n\e[22m","\e[3m\e[1m[i] You can't select this option...\e[0m\n","E00:INCORRECT_SCENE_INTEGER","E01:INVALID_INTEGER","\e[48;5;90m\e[38;5;21m\e[1mThanks for trying the prototype! Unfortunately, the fun ends here, as I still have a lot of coding to do. Be sure to check the links below every now and then to see when a new update comes out!\n\nCheck development at:\n\thttps://github.com/polygonnedpotato/nord\n\nSee you soon,\n\t--Discovery.\n\e[0m"};
+    const string scn_00[2]={"\e[36m[?]Hey...\n[?]Hey!\n\e[1m[?]HEY!!!\n\e[0m\e[35m<You wake up suddenly.>\n<You look up and notice a larg dark figure. It speaks in a masculine voice...>\e[0m\e[36m\n[?]Come with me,\e[1m now\e[22m.\n\e[0m","\e[32m[1]O-Okay...\n\e[31m[2]Umm... No?\e[0m\n[3]Who are you?\n"};
+    const string scn_01[2]={"\e[36m[?]I'm the guy who's trying to save you. Now follow me!\n\e[0m","\e[32m[1]Okay\n\e[31m[2]No.\n\e[0m\e[2m[3]-----\n\e[0m"};
+    const string scn_02[2]={"\e[36m[?]Are you joking? Now is not the time for this! Especially considering that \e[1mthe sweep\e[22m is happening. Now come on!\e[0m\n","\e[32m[1]Okay...\n\e[0m[2]Wait what's 'the sweep'?\n[3]Who are you?\n"};
+    const string scn_03[2]={"\e[35m<The dark figure pulls you up without patience.>\n<You find it hard to stabalize yourself on your feet.>\n<Without hesitation, the figure grabs ahold of your arm and pulls you along with itself.>\n<As the figure pulls you to an undisclosed location, you look around. Destrucion, fire, debris. it looks as if the entire place was severly bombed.>\n<You feel light headed.>\n<You blackout.>\e[0m\n","\e[33m[1]Continue\e[0m\n\e[2m[2]-----\n[3]-----\e[0m\n"};
+    const string scn_04[2]={"\e[35m<...>"};
+    const string scn_hs[2]={"\e[35mPo twojej pysznej zupie\nNie ruszam dupy z klopa\nTa zupa było z mlekiem;\nNa mleko mam alergię\e[0m\n","[1]what\n[2]????\n[3]przepraszam"};
 };
 RES obj;
 int nord;
@@ -27,7 +29,7 @@ void switchScene(int scn,int choice){
           break;
         default:
           cerr<<obj.game_dat[3];
-          exit(EXIT_FAILURE);
+          exit(1);
       }
       break;
     case 1: //01
@@ -43,7 +45,7 @@ void switchScene(int scn,int choice){
           break;
         default:
           cerr<<obj.game_dat[3];
-          exit(EXIT_FAILURE);
+          exit(1);
       };
       break;
     case 2: //02
@@ -60,7 +62,7 @@ void switchScene(int scn,int choice){
           break;
         default:
           cerr<<obj.game_dat[3];
-          exit(EXIT_FAILURE);
+          exit(1);
       }
       break;
     case 3: //03
@@ -76,12 +78,27 @@ void switchScene(int scn,int choice){
           break;
         default:
           cerr<<obj.game_dat[3];
-          exit(EXIT_FAILURE);
+          exit(1);
       }
       break;
+    case 4: //04
+      switch(choice){
+        case 1:
+          cout<<obj.game_dat[4];
+          exit(0);
+        case 2:
+          cout<<obj.game_dat[1];
+          break;
+        case 3:
+          cout<<obj.game_dat[1];
+          break;
+        default:
+          cerr<<obj.game_dat[3];
+          exit(1);
+      }
     default:
       cerr<<obj.game_dat[2];
-      exit(EXIT_FAILURE);
+      exit(1);
   }
 };
 void printScene(int scn){
@@ -98,13 +115,16 @@ void printScene(int scn){
     case 3: //03
       cout<<obj.scn_03[0]<<obj.game_dat[0]<<obj.scn_03[1];
       break;
+    case 4: //04
+      cout<<obj.scn_04[0]<<obj.game_dat[0]<<obj.scn_04[1];
+      break;
     default:
       cerr<<obj.game_dat[2];
-      exit(EXIT_FAILURE);
+      exit(1);
   };
 };
 int main() {
-  cout << "█   █  ███  ████  ████\n██  █ █   █ █   █ █   █\n█ █ █ █   █ ████  █   █\n█  ██ █   █ █   █ █   █\n█   █  ███  █   █ ████\nBy Brendan Berger\nVersion 0.0\n\n\n";
+  cout<<"█   █  ███  ████  ████\n██  █ █   █ █   █ █   █\n█ █ █ █   █ ████  █   █\n█  ██ █   █ █   █ █   █\n█   █  ███  █   █ ████\nBy Brendan Berger\nVersion 0.0\n\n\n";
   currscne=0;
   while(keepGoing){
     printScene(currscne);
